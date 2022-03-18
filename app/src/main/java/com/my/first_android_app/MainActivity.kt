0dc81f738aa838aaf8cbb5f27bd2840651361395
nald-dev <rynvva@gmail.com>
2022-03-19 05:50:17 +0700

    Add image loader

--- app/src/main/java/com/my/first_android_app/MainActivity.kt
+++ app/src/main/java/com/my/first_android_app/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.my.first_android_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+// import com.bumptech.glide.Glide
+import com.squareup.picasso.Picasso
+
+
+class MainActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.imageView3)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            // Glide.with(this).load("http://goo.gl/gEgYUd").into(imageView)
+
+            Picasso.get().load("https://i.imgur.com/DvpvklR.png").into(imageView)
+        }
     }
 }
